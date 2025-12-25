29975f56def450271488077c5e2301293b3d0ca1
Youssef Abdelouali <132459755+Youssef-abdelouali@users.noreply.github.com>
2025-12-25 15:53:56 +0100

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import NavBar from "./Components/NavBar";
-
+import Hero from "./Components/Hero";
 const App = () => {
 	return (
 		<main  className="bg-gray-950">
 			<NavBar/>
-
+            <Hero/>
 		</main>
 	);
 };
