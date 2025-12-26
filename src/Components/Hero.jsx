e4075199ace6ae59e13e736ea84ec1aec943bc30
Youssef Abdelouali <132459755+Youssef-abdelouali@users.noreply.github.com>
2025-12-26 11:21:41 +0100

    aadded

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,12 +1,20 @@
-
+import React, { useRef, useEffect } from 'react';
 const Hero = () => {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.playbackRate = 2;
+    }
+  }, []);
+
   return (
     <section className="text-center text-white py-30 flex flex-col items-center gap-10 bg-black">
         <div className="flex flex-col gap-6">
             <h1 className="text-5xl">MacBook Pro</h1>
             <img src="/title.png" alt="MacBook Pro Title"  className="w-200"/>
         </div>
-        <video src="/videos/hero.mp4" autoPlay loop muted className="w-full max-w-5xl rounded-lg shadow-lg"/>
+        <video ref={videoRef} src="/videos/hero.mp4" autoPlay loop muted className="w-full max-w-5xl rounded-lg shadow-lg"/>
 
     </section>
   )
