a8db4fc21f103be9063d2a405239a3e3a7980b13
Youssef Abdelouali <132459755+Youssef-abdelouali@users.noreply.github.com>
2025-12-27 17:05:04 +0100

    three js added

--- src/Components/Productviewer.jsx
+++ src/Components/Productviewer.jsx
@@ -1,11 +1,15 @@
+/* eslint-disable no-irregular-whitespace */
 import React from "react";
 import useMacbookStore from "../store";
+import {Canvas} from "@react-three/fiber";
+import {Box,OrbitControls} from "@react-three/drei";
+
 import clsx from "clsx";
 
 const Productviewer = () => {
-    const { color , scale, setColor, setScale}= useMacbookStore((state) => ({ color: state.color, scale: state.scale, setColor: state.setColor, setScale: state.setScale, reset: state.reset }));
+const { color, scale, setColor, setScale } = useMacbookStore();
 
-  return ( 
+  return (
     <section
       className="text-white bg-black w-[90%] mx-auto"
       id="product-viewer"
@@ -13,26 +17,55 @@ const Productviewer = () => {
       <h2 className="text-5xl">Take a closer look.</h2>
 
       <div className="controls">
-        <p>MacBook Pro 16” in Space Black</p>
+        
+        <p>MacBook Pro {scale === 0.06 ? '14"' : '16"'} in {color}</p>
         <div className="flex-center gap-5 mt-5">
           <div className="color-control">
-            <div onClick={() => setColor("#adb5BD")} 
-                 className={clsx("bg-neutral-300", color === "#adb5BD" && 'active')} />
-            <div onClick={() => setColor("#2E2C2E")} 
-                 className={clsx("bg-neutral-900", color === "#2E2C2E" && 'active')} />
+            <div
+              onClick={() => setColor("#adb5bd")}
+              className={clsx(
+                "bg-neutral-300",
+                color === "#adb5bd" && "active"
+              )}
+            />
+            <div
+              onClick={() => setColor("#2e2c2e")}
+              className={clsx(
+                "bg-neutral-900",
+                color === "#2e2c2e" && "active"
+              )}
+            />
           </div>
-          <div className="size-control"> 
-            <div>
-              <p>16"</p>
-            </div>
-            <div>
+          <div className="size-control">
+            <div
+              onClick={() => setScale(0.06)}
+              className={clsx(
+                scale === 0.06
+                  ? "bg-white text-black"
+                  : "bg-transparent text-white"
+              )}
+            >
               <p>14"</p>
+            </div>
+            <div
+              onClick={() => setScale(0.08)}
+              className={clsx(
+                scale === 0.08
+                  ? "bg-white text-black"
+                  : "bg-transparent text-white"
+              )}
+            >
+              <p>16"</p>
             </div>
           </div>
         </div>
       </div>
 
-      <p className="text-white text-4xl">Render canvas</p>
+      <Canvas  id="canvas"   camera={{position:[0,2,5], fov:50, near:0.1, far:100}}>
+        <Box positon={[0, 0, 0 ]} scale={10*scale} material-color={color}/>
+        <OrbitControls enableZoom={false} />
+      </Canvas>
+
     </section>
   );
 };
